f77d39f4a1665097952119b328208436ffa868a0
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2011-11-26 23:49:44 +0100

    removed unnecessary steps

--- features/step_definitions/example_steps.rb
+++ features/step_definitions/example_steps.rb
@@ -1,3 +1,19 @@
 Given /^I jump up and down with joy$/ do
   # Wheee!
 end
+
+Given /^(?:|I )am on (.+)$/ do |page|
+  visit path_to(page)
+end
+
+When /^(?:|I )fill in "([^\"]*)" with "([^\"]*)"$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+When /^(?:|I )press "([^\"]*)"$/ do |button|
+  click_button(button)
+end
+
+Then /^(?:|I )should see "([^\"]*)"$/ do |text|
+  page.should have_content(text)
+end
