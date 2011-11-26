6ac23a3a0f394aeea75b68b156c419ed3e7d9604
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2011-11-26 23:59:36 +0100

    clean up configuration

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require 'capybara/cucumber'
-
 require File.expand_path(File.join(File.dirname(__FILE__), "paths"))
 
+require 'capybara/cucumber'
 require 'capybara-webkit'
 Capybara.default_driver = :webkit
